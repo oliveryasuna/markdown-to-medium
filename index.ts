2d2fd586c49d2f17b193dfa4386c0c384718a9a1
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2026-07-07 10:59:22 -0400

    #4. Prepared for publishing.

--- index.ts
+++ index.ts
@@ -1,34 +1,16 @@
-#!/usr/bin/env bun
+#!/usr/bin/env node
 
-import {parseArgs as nodeParseArgs} from 'node:util';
 import {marked} from 'marked';
 
-interface Args {
-  write?: string;
-}
-
-function parseArgs(argv: string[]): Args {
-  try {
-    const {values} = nodeParseArgs({
-      args: argv,
-      options: {
-        'write': {type: 'string'},
-      },
-      strict: true
-    });
-
-    return {write: values.write};
-  } catch(e) {
-    console.error((e as Error).message);
-    console.error('Usage: echo "<markdown>" | m2m [--write <file>]');
-    process.exit(1);
-  }
-}
-
 async function readStdin(): Promise<string> {
-  const markdown = await Bun.stdin.text();
+  let markdown = '';
+  process.stdin.setEncoding('utf8');
+  for await (const chunk of process.stdin) {
+    markdown += chunk;
+  }
+
   if(!markdown) {
-    console.error('Usage: echo "<markdown>" | m2m [--write <file>]');
+    console.error('Usage: echo "<markdown>" | m2m');
     process.exit(1);
   }
 
@@ -39,16 +21,7 @@ async function convertToHtml(markdown: string): Promise<string> {
   return await marked(markdown);
 }
 
-async function writeToFile(html: string, path: string): Promise<void> {
-  await Bun.write(path, html);
-}
-
-const args = parseArgs(process.argv.slice(2));
 const markdown = await readStdin();
 const html = await convertToHtml(markdown);
 
-if(args.write) {
-  await writeToFile(html, args.write);
-} else {
-  process.stdout.write(html);
-}
+process.stdout.write(html);
